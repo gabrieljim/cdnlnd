505e821887ca7614e3a6b4fbccf72efab51f81f8
gabrieljim <gabrieljim641@gmail.com>
2020-07-16 22:29:12 -0400

    added footer structure

--- client/src/components/Footer/index.js
+++ client/src/components/Footer/index.js
@@ -3,12 +3,52 @@ import * as SC from "./styles";
 
 const Footer = () => {
 	return (
-		<SC.Section>	
+		<SC.Section>
 			<SC.Content>
-				<h1>CDNLND</h1>
+				<SC.Column>
+					<SC.Header>
+						Lorem
+					</SC.Header>
+				</SC.Column>
+				<SC.Column>
+					<SC.Header>
+						Lorem
+					</SC.Header>
+					<SC.Item>
+						Ipsum
+					</SC.Item>
+					<SC.Item>
+						Ipsum
+					</SC.Item>
+					<SC.Item>
+						Ipsum
+					</SC.Item>
+				</SC.Column>
+				<SC.Column>
+					<SC.Header>
+						Lorem
+					</SC.Header>
+					<SC.Item>
+						Ipsum
+					</SC.Item>
+					<SC.Item>
+						Ipsum
+					</SC.Item>
+					<SC.Item>
+						Ipsum
+					</SC.Item>
+					<SC.Item>
+						Ipsum
+					</SC.Item>
+				</SC.Column>
 			</SC.Content>
-		</SC.Section>	
-	)		
-}
+			<SC.Content>
+				<SC.Copyright>
+					Copyright &copy; {new Date().getFullYear()} CDNLND
+				</SC.Copyright>
+			</SC.Content>
+		</SC.Section>
+	);
+};
 
 export default Footer;
--- client/src/components/Footer/styles.js
+++ client/src/components/Footer/styles.js
@@ -1,25 +1,40 @@
 import styled from "styled-components";
 
 export const Section = styled.section`
-	padding-top: ${props => props.theme.navHeight}px;
-	padding-bottom: ${props => props.theme.navHeight}px;
 	min-height: 30vh;
 	width: 100%;
 	background: ${props => props.theme.dark};
 	color: ${props => props.theme.text};
+	padding: ${props => props.theme.navHeight}px 30px;
 	display: flex;
-	align-items: center;
+	align-items: flex-start;
+	justify-content: space-between;
 	position: relative;
+	flex-direction: column;
 `;
 
 export const Content = styled.div`
 	width: 100%;
+	text-align: left;
 	display: flex;
-	justify-content: space-evenly;
-	flex-direction: column;
-	padding: 0 10%;
+	justify-content: flex-start;
+`
 
-	@media screen and (max-width: 800px) {
-		justify-content: center;
-	}
+export const Header = styled.div`
+	font-size: 18px;
+	font-weight: bold;
+`
+
+export const Item = styled.p`
+	font-size: 16px;
+`
+
+export const Column = styled.div`
+	display: flex;
+	flex-direction: column;
+	margin-right: 20px;
+`
+
+export const Copyright = styled.p`
+	margin-top: 20px;
 `
